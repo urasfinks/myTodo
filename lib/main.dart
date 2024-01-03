06f09b9a5d916f22273a10e943c2689a1a443e43
sfinks <urasfinks@yandex.ru>
2024-01-03 17:19:14 +0300

    Android настройка push notification

--- lib/main.dart
+++ lib/main.dart
@@ -1,25 +1,49 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
+
 import 'package:my_todo/push_notification.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await PushNotification.init();
+  PushNotification();
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
+  final bool _running = true;
+
+  Stream<String> _clock() async* {
+    // This loop will run forever because _running is always true
+    while (_running) {
+      await Future<void>.delayed(const Duration(seconds: 1));
+
+      yield PushNotification().list.join("\n\n");
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
-    PushNotification.getToken();
-
+    PushNotification().getToken();
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
           title: const Text('Push Example App'),
         ),
-        body: const SizedBox(),
+        body: StreamBuilder(
+          stream: _clock(),
+          builder: (context, AsyncSnapshot<String> snapshot) {
+            if (snapshot.connectionState == ConnectionState.waiting) {
+              return const CircularProgressIndicator();
+            }
+            return Text(
+              snapshot.data!,
+              style: const TextStyle(fontSize: 14, color: Colors.blue),
+            );
+          },
+        ),
       ),
     );
   }
--- lib/push_notification.dart
+++ lib/push_notification.dart
@@ -3,56 +3,105 @@ import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 import 'dart:io';
 import 'package:push/push.dart' as push;
 import 'package:push/push.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_messaging/firebase_messaging.dart' as fcm;
 
-enum PushType { onToken, onNotificationTap, onMessage, onBackgroundMessage }
+@pragma('vm:entry-point')
+Future<void> _firebaseMessagingBackgroundHandler(fcm.RemoteMessage message) async {
+  await Firebase.initializeApp();
+  fcm.RemoteNotification? notification = message.notification;
+  fcm.AndroidNotification? android = message.notification?.android;
+  if (notification != null && android != null && notification.title != null && notification.body != null) {
+    PushNotification().openPush(notification.title!, notification.body!);
+    Map<String, Object> result = {
+      "information": notification.body!,
+      "data": message.data["message"] ?? "",
+    };
+    if (result["data"] != "") {
+      PushNotification().onMessage(result, PushType.onCloseApplication);
+    }
+  }
+}
+
+enum PushType { onToken, onNotificationTap, onMessage, onBackgroundMessage, onCloseApplication }
 
 class PushNotification {
-  static FlutterLocalNotificationsPlugin? plugin;
-
-  static AndroidNotificationChannel mainChannel = const AndroidNotificationChannel(
-    'main',
-    'main',
-    description: 'Main chanel',
+  static final PushNotification _singleton = PushNotification._internal();
+  FlutterLocalNotificationsPlugin? _plugin;
+  bool _isInit = false;
+  AndroidNotificationChannel channel = const AndroidNotificationChannel(
+    'high_importance_channel',
+    'High Importance Notifications',
+    description: 'This channel is used for important notifications.',
     importance: Importance.max,
   );
 
-  static Future<void> init() async {
-    plugin = await PushNotification.initPlugin();
+  factory PushNotification() {
+    if (_singleton._isInit == false) {
+      _singleton._isInit = true;
+      _singleton.init();
+    }
+    return _singleton;
+  }
 
+  PushNotification._internal();
+
+  List<String> list = [];
+
+  void onMessage(Map<String?, Object?>? data, PushType type) {
+    list.add("type: ${type.name}; data: $data");
+    print("onMessage($type) $data");
+  }
+
+  init() {
+    initPlugin().then((value) {
+      _plugin = value;
+    });
+    if (Platform.isAndroid) {
+      //Регистрация закрытого обработчика
+      //fcm.FirebaseMessaging.onBackgroundMessage(_firebaseMessagingBackgroundHandler);
+      FlutterLocalNotificationsPlugin()
+          .resolvePlatformSpecificImplementation<AndroidFlutterLocalNotificationsPlugin>()!
+          .createNotificationChannel(channel);
+    }
     Push.instance.onNewToken.listen((token) => onMessage({"token": token}, PushType.onToken));
     Push.instance.onNotificationTap.listen((data) => parseNotificationTap(data));
-    Push.instance.onMessage.listen((message) => onMessage(parseRemoteMessage(message), PushType.onMessage));
-    Push.instance.onBackgroundMessage
-        .listen((message) => onMessage(parseRemoteMessage(message), PushType.onBackgroundMessage));
+    Push.instance.onMessage.listen((message) => parseRemoteMessage(message, PushType.onMessage));
+    Push.instance.onBackgroundMessage.listen((message) => parseRemoteMessage(message, PushType.onBackgroundMessage));
+  }
 
-    if (!Platform.isAndroid) {
-      return;
+  void parseNotificationTap(Map<String?, Object?> data) {
+    Map<String, Object> result = {"information": "", "data": ""};
+    if (Platform.isIOS) {
+      result["information"] = selector(data, "aps.alert", "");
+      if (result["information"] == "") {
+        result["information"] = selector(data, "title", "");
+      }
+      result["data"] = selector(data, "message", "");
     }
-
-    await FlutterLocalNotificationsPlugin()
-        .resolvePlatformSpecificImplementation<AndroidFlutterLocalNotificationsPlugin>()!
-        .createNotificationChannel(mainChannel);
+    if (result["data"] != "") {
+      onMessage(result, PushType.onNotificationTap);
+    }
   }
 
-  static void displayForegroundNotification(push.Notification notification) async {
-    final androidOptions = AndroidNotificationDetails(
-      mainChannel.id,
-      mainChannel.name,
-      channelDescription: mainChannel.description,
-      importance: Importance.max,
-      priority: Priority.high,
-      ticker: "A manually-sent push notification.",
-      styleInformation: const DefaultStyleInformation(
-        false,
-        false,
-      ),
-    );
-    const iosOptions = DarwinNotificationDetails(presentAlert: true, presentBadge: true, presentSound: true);
-    final platformChannelSpecifics = NotificationDetails(android: androidOptions, iOS: iosOptions);
-    await plugin!.show(0, notification.title, notification.body, platformChannelSpecifics);
+  void parseRemoteMessage(push.RemoteMessage message, PushType pushType) {
+    Map<String, Object> result = {"information": "", "data": ""};
+    if (message.notification != null && message.notification!.title != null && message.notification!.body != null) {
+      openPush(message.notification!.title!, message.notification!.body!);
+    }
+    if (Platform.isIOS) {
+      result["information"] = selector(message.data, "aps.alert", "");
+      result["data"] = selector(message.data, "message", "");
+    } else if (Platform.isAndroid) {
+      result["information"] = message.notification?.body ?? "";
+      result["data"] = message.data?["message"] ?? "";
+    }
+    if (result["data"] != "") {
+      onMessage(result, pushType);
+    }
   }
 
-  static void getToken() {
+  void getToken() {
     if (Platform.isIOS) {
       Push.instance.getNotificationSettings().then((settings) {
         if (settings.authorizationStatus == UNAuthorizationStatus.authorized) {
@@ -76,11 +125,31 @@ class PushNotification {
     }
   }
 
-  static void onMessage(Map<String?, Object?>? data, PushType type) {
-    print("onMessage($type) $data");
+  void openPush(String title, String body) async {
+    final androidOptions = AndroidNotificationDetails(
+      channel.id,
+      channel.name,
+      channelDescription: channel.description,
+      importance: Importance.max,
+      priority: Priority.high,
+      ticker: "A manually-sent push notification.",
+      styleInformation: const DefaultStyleInformation(
+        false,
+        false,
+      ),
+    );
+    const iosOptions = DarwinNotificationDetails(presentAlert: true, presentBadge: true, presentSound: true);
+    final platformChannelSpecifics = NotificationDetails(android: androidOptions, iOS: iosOptions);
+    await _plugin!.show(
+      0,
+      title,
+      body,
+      platformChannelSpecifics,
+      payload: 'Default_Sound',
+    );
   }
 
-  static Future<FlutterLocalNotificationsPlugin> initPlugin() async {
+  Future<FlutterLocalNotificationsPlugin> initPlugin() async {
     final flutterLocalNotificationsPlugin = FlutterLocalNotificationsPlugin();
     const DarwinInitializationSettings settingsApple = DarwinInitializationSettings(
       requestAlertPermission: true,
@@ -95,31 +164,7 @@ class PushNotification {
     return flutterLocalNotificationsPlugin;
   }
 
-  static void parseNotificationTap(Map<String?, Object?> data) {
-    Map<String, Object> result = {"information": "", "data": ""};
-    if (Platform.isIOS) {
-      result["information"] = selector(data, "aps.alert", "");
-      if (result["information"] == "") {
-        result["information"] = selector(data, "title", "");
-      }
-      result["data"] = selector(data, "message", "");
-    }
-    onMessage(result, PushType.onNotificationTap);
-  }
-
-  static Map<String, Object> parseRemoteMessage(RemoteMessage message) {
-    Map<String, Object> result = {"title": "", "data": ""};
-    if (message.notification != null) {
-      displayForegroundNotification(message.notification!);
-    }
-    if (Platform.isIOS) {
-      result["information"] = selector(message.data, "aps.alert", "");
-      result["data"] = selector(message.data, "message", "");
-    } else if (Platform.isAndroid) {}
-    return result;
-  }
-
-  static dynamic selector(Map<String?, dynamic>? data, String path, [dynamic defaultValue]) {
+  dynamic selector(Map<String?, dynamic>? data, String path, [dynamic defaultValue]) {
     defaultValue ??= "[$path]";
     if (data == null) {
       return defaultValue;
@@ -139,7 +184,7 @@ class PushNotification {
           break;
         }
       }
-    } catch (e, stacktrace) {
+    } catch (e) {
       find = false;
     }
     if (!find) {
